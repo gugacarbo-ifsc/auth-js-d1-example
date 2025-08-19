91db498f3deba2d6cc7c3c566e98893e98ac19d0
alunolages <alunolages@lages.sc>
2025-08-19 10:50:44 -0300

    test

---
+++ src/app/api/up-db/route.ts
@@ -0,0 +1,14 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { up } from "./up";
+
+export async function GET() {
+    try {
+        await up((await getCloudflareContext({ async: true })).env.DB)
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            const causeMessage = e.cause instanceof Error ? e.cause.message : String(e.cause);
+            console.log(causeMessage, e.message)
+        }
+    }
+    return new Response('Migration completed');
+}
